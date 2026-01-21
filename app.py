bd7f89800a2bea1ebfc059a5f0b54d80a0bd7a28
Arsen <arsen.makaov@gmail.com>
2026-01-21 18:07:10 +0300

    cpu

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from audio_recorder_streamlit import audio_recorder
 #     unsafe_allow_html=True,
 # )
 # Configuration for faster-whisper
-DEVICE = "cuda"           # "cuda" if you have GPU, else "cpu"
+DEVICE = "cpu"           # "cuda" if you have GPU, else "cpu"
 COMPUTE_TYPE = "float16"  # fast on GPU; if CPU, try "int8" or "int8_float16"
 LANGUAGE = "ar"           # e.g. "ar" for Arabic, "en" for English, or None to auto-detect
 TASK = "transcribe"       # or "translate"
